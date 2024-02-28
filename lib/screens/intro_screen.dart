b1046746baa2d800e452acc2401204e24287399c
FieTengberg <fietengberg@hotmail.com>
2024-02-28 15:45:25 +0100

    størrelsesændring til tablet

--- lib/screens/intro_screen.dart
+++ lib/screens/intro_screen.dart
@@ -14,29 +14,29 @@ class IntroScreen extends StatelessWidget {
             // Large square for text
             Container(
               width: 600, // Adjust size as needed
-              height: 200, // Adjust size as needed
+              height: 300, // Adjust size as needed
               decoration:
                   BoxDecoration(border: Border.all(color: Colors.black)),
               alignment: Alignment.center,
               child: Padding(
-                padding: const EdgeInsets.all(10.0),
+                padding: const EdgeInsets.all(30.0),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Text(
                       'Hej Jan!',
-                      style: TextStyle(fontSize: 14),
+                      style: TextStyle(fontSize: 20),
                     ),
                     SizedBox(height: 10), // Add some space between sections
                     Text(
                       'For at hjælpe din fysioterapeut med at målrette din behandlingsindsats, skal vi have dig til at besvare nogle spørgsmål omhandlende din problematik og grunden til at du er her.',
-                      style: TextStyle(fontSize: 14),
+                      style: TextStyle(fontSize: 20),
                     ),
                   ],
                 ),
               ),
             ),
-            SizedBox(height: 20), // Spacer
+            SizedBox(height: 30), // Spacer
             // Pressable button
             ElevatedButton(
               onPressed: () {
@@ -44,11 +44,11 @@ class IntroScreen extends StatelessWidget {
               },
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.blue,
-                minimumSize: Size(200, 50),
+                minimumSize: Size(450, 50),
               ),
               child: Text(
                 'Start',
-                style: TextStyle(color: Colors.white),
+                style: TextStyle(color: Colors.white, fontSize: 20),
               ),
             ),
           ],
