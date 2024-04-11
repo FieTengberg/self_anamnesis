69c21c7c76c8c27d66c45b83efbc4fb6f0e64b6b
Alona90 <DS26DD@student.aau.dk>
2024-04-11 11:27:21 +0200

    With "datatype" var

--- python/text_gen.py
+++ python/text_gen.py
@@ -3,12 +3,12 @@ import os
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
-torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+datatype = torch.float16 if torch.cuda.is_available() else torch.float32
 
 model_id = "openai/whisper-large-v3"
 
 model = AutoModelForSpeechSeq2Seq.from_pretrained(
-    model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+    model_id, torch_dtype=datatype, low_cpu_mem_usage=True, use_safetensors=True
 )
 model.to(device)
 
@@ -25,7 +25,7 @@ pipe = pipeline(
     return_timestamps=True,
     # not necessary to successfully running our code
     # but included to ensure compatibility with the model's input
-    torch_dtype=torch_dtype,
+    torch_dtype=datatype,
     device=device,
 )
 
